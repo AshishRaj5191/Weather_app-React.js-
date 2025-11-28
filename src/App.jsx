ccb898251361008dfc66f6048523449b4faf6ee8
AshishRaj5191 <ashish.ara2017@gmail.com>
2025-11-28 22:52:53 +0530

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Fetch from './Fetch'
+import WeatherApp from './WeatherApp'
+
+const App = () => {
+  return (
+          
+      <WeatherApp/>
+
+  )
+}
+
+export default App
